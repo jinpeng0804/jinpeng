0bd1842c95d1f44032488b6273f05b05b57b0c3a
jinpeng0804 <jinpeng0804@vip.qq.com>
2018-09-16 10:12:19 +0800

    merge-git
    merge-git

--- testdata-jin/main/main.go
+++ testdata-jin/main/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	// testSet := testdata.Tests{}
+	// jinpeng := testdata.Tests{}
 	// a := data.Struct1{}
 	//a := testdata.GetTestDataSet(&testdata.Type1{}, testdata.ErrorManIsTure)
 	a := testdata.GetTestDataSet(&testdata.Type1{}, testdata.ErrorManIsFalse)
